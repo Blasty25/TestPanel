cc7bc1ee4a93cc0d1e9465eadbf45494fb467204
Blasty25 <sohamshah210@gmail.com>
2025-03-20 14:09:55 -0400

    annoying

---
+++ src/main/java/frc/robot/Subsystems/Elevator/Commands/Autovator.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.elevator.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.elevator.Elevator;
+import frc.robot.Subsystems.elevator.Elevator.ElevatorSetpoint;
+import org.littletonrobotics.junction.Logger;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Autovator extends Command {
+  public Elevator elevator;
+
+  public Autovator(Elevator elevator) {
+    this.elevator = elevator;
+  }
+
+  @Override
+  public void initialize() {
+    Logger.recordOutput("Elevator/Auto/Setpoint", elevator.getSetpoint());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    elevator.setSetpoint(() -> ElevatorSetpoint.L4);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
@@ -1,58 +1,46 @@
-package frc.robot.Subsystems.Elevator;
+package frc.robot.Subsystems.elevator;
 
-import static edu.wpi.first.units.Units.Meters;
-
-import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.DistanceUnit;
-import edu.wpi.first.units.measure.Distance;
 
 public class ElevatorConstants {
-    public static final DCMotor motor = DCMotor.getNEO(2);
+  public static final int spark = 10;
+  public static final int followerSpark = 11;
 
-    public static final double mass = Units.lbsToKilograms(25.1); // change to actual weight
-    public static final double radius = 5.0 / 1000.0 * 36 / (2.0 * Math.PI);
-    public static final double gearing = (5.0 / 1.0);
+  public static final double travel = Units.inchesToMeters(69.0);
+  public static final double mass = Units.lbsToKilograms(25.1);
 
-    public static final double maxHeight = Units.inchesToMeters(44);
-    public static final double minHeight = Units.inchesToMeters(0.0);
-    public static final double startingHeight = minHeight;
+  public static final double stageOneTravel = Units.inchesToMeters(24);
+  public static final double stageTwoTravel = Units.inchesToMeters(24);
+  public static final double carriageTravel = Units.inchesToMeters(21);
 
-    // sim
+  public static final double drumRadius = 5.0 / 1000.0 * 36 / (2.0 * Math.PI);
+  public static final double gearing = (9.0 / 1.0);
+  public static final double positionConversionFactor = drumRadius * 2 * Math.PI / gearing;
+  public static final double tolerance = .005;
 
-    // public static final double p = 23.918;
-    // public static final double i = 0.0;
-    // public static final double d = 0.45168;
+  public static final int current = 60;
 
-    // public static final double s = 0.0;
-    // public static final double g = 1.0612;
-    // public static final double v = 5.87;
-    // public static final double a = 0.12;
+  public static class Gains {
+    public static final double[] kS = {0.6, 0.6, 0.9};
+    public static final double[] kG = {0.5, 0.5, 0.5};
+    public static final double kV = 6;
+    public static final double[] kA = {0.0, 0.0, 0.0};
 
-    public static final double p = 0.0;
-    public static final double i = 0.0;
-    public static final double d = 0.0;
+    public static final double kP = 40.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+  }
 
-    public static final double s = 0.27117;
-    public static final double g = 0.42059;
-    public static final double v = 6.049;
-    public static final double a = 0.75;
+  public static final double maxVelocity = // 0.3;
+      1.8;
+  public static final double maxAcceleration = // 0.3;
+      25.5;
 
-    public static final double maxProfileVelocity = 0.5;
-    public static final double maxProfileAcceleration = 0.5;
+  public static final double homingVolts = -4.0;
+  public static final double homingTimeSecs = 0.2;
+  public static final double homingVelocityThresh = 0.2;
 
-    public static final double maxProfileVoltage = 6.0;
+  public static final double staticCharacterizationVelocityThresh = 0.1;
 
-    public static final int leftMotorId = 10;
-    public static final int rightMotorId = 11;
-
-    public static final double positionConversionFactor = radius * 2 * Math.PI / gearing;
-    public static final double velocityConversionFactor = positionConversionFactor / 60.0;
-
-    public static final Distance sysIdMinPosition = Distance.ofBaseUnits(0.1, Meters);
-    public static final Distance sysIdMaxPosition = Distance.ofBaseUnits(1.5, Meters);
-
-    public static final double sysIdRampUp = 2.5;
-    public static final double sysIdStep = 5.5;
-    public static final double sysIdTimeout = 20.0;
+  public static final double visualizerOffset = 0.0;
 }
